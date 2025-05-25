5d14e2eec2ee195f5d2a4964ce886684a1e78a01
TestaMic <mateidiac9@gmail.com>
2025-05-25 09:49:48 +0300

    Update build.sh - clang 18 works too

--- build.sh
+++ build.sh
@@ -29,7 +29,7 @@ export KBUILD_BUILD_USER="TestaMic"
 # Clang
 clangbin=../clang/bin/clang
 if ! [ -a $clangbin ]; then
-wget -O toolchain.tar.gz https://android.googlesource.com/platform/prebuilts/clang/host/linux-x86/+archive/refs/heads/android14-release/clang-r487747c.tar.gz
+wget -O toolchain.tar.gz https://android.googlesource.com/platform/prebuilts/clang/host/linux-x86/+archive/refs/heads/main/clang-r547379.tar.gz
 
 mkdir -p ${PWD}/../clang
 tar -xzf toolchain.tar.gz -C ${PWD}/../clang
